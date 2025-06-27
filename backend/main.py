311a42f80dd8b9b9d09591ee42be6707777cc90a
CSzzs <chandima.senarathna97@gmail.com>
2025-06-27 21:00:44 +0530

    main.py programmed

--- backend/main.py
+++ backend/main.py
@@ -25,4 +25,34 @@ if not DATABASE_URL:
     logging.critical("DATABASE_URL environment variable not set. Application cannot start.")
     
 # ---API Endpoints---
-@app.get
+@app.get("/", tags=["Health Check"])
+def health_check(response: Response):
+    """
+    Checks the Helath of the API and its database connection
+    """
+    conn = None
+    try:
+        if not DATABASE_URL:
+            raise ValueError("Database URL is Not configured.")
+        
+        logging.info("Attempting to connect to the databse...")
+        conn = psycopg2.connect(DATABASE_URL)
+        
+        logging.info("Database connection succesful.")
+        
+        return{"status": "success", "message": "API is running and database connection is helthy."}
+    
+    except psycopg2. OperationalError as e:
+        logging.error(f"Database operational error: {e}")
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return {"status": "error", "message": "Could not connect to the database"}
+    
+    except Exception as e:
+        logging.error(f"An unexepected error occured during health check: {e}")
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return{"status": "error", "message": "An internal server error occured."}
+    
+    finally:
+        if conn is not Nonel:
+            conn.close()
+            logging.info("Database connection closed")
