842b66290140fd94380f2cf2a1fd46fb5c6a84ea
CSzzs <chandima.senarathna97@gmail.com>
2025-07-14 19:34:43 +0530

    auth.py were updated with new database.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,16 +1,17 @@
 import os
 from datetime import datetime, timedelta, timezone
+
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+
 import schemas
+from database import get_db_connection
 
 # Load envirnment varibles
 load_dotenv()
-
-oauth2_sheme = OAuth2PasswordBearer(tokenUrl="login")
 
 #Loading environment variables
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
@@ -19,6 +20,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 # Reusing the password context from main.py
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#OAuth2 scheme to extract the toke from the "Authorizatin : Bearer <token> header"
+oauth2_sheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def verify_password(plain_password: str, hashed_password:str) -> bool:
     """Verifies a plain password against a hashed_one"""
@@ -32,7 +36,11 @@ def create_access_token(data: dict) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def get_current_user(token: str = Depends(oauth2_sheme), db: Connection = Depends("...")) ->schemas.UserResponse:
+def get_current_user(
+    token: str = Depends(oauth2_sheme), db:connection = Depends("get_db_connection")) ->schemas.UserResponse:
+    """
+    Dependency function to secure endpoints
+    """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail= "Could not validate credentials",
@@ -40,10 +48,13 @@ def get_current_user(token: str = Depends(oauth2_sheme), db: Connection = Depend
     )
     
     try:
+        # Decode the Code to get payload
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: int = payload.get("user_id")
         if user_id is None:
             raise credentials_exception
+        
+        token_data = schemas.TokenData(user_id=user_id)
         
     except JWTError:
         raise credentials_exception
