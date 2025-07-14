d8628a6acea5e77e0b0b5359cc98330989fe5c09
CSzzs <chandima.senarathna97@gmail.com>
2025-07-14 19:33:09 +0530

    isolates all the database logic in database.py

--- backend/database.py
+++ backend/database.py
@@ -1,2 +1,34 @@
-# This file will contain all the database connection and session logic.
-print("Database module loaded")
+import os
+import logging
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from psycopg2.extras import RealDictCursor
+
+# Loading the environment variables
+load_dotenv()
+DTABASE_URL = os.getenv("DATABASE_URL")
+
+# Creating the SQLAlchemy engine
+engine = create_engine(DTABASE_URL)
+
+def get_db_connection():
+    """
+    Dependency that creates and yields a new raw database conection and cursor per request.
+    This ensures the connection is always commited and closed properly
+    """
+    
+    db_conn = None
+    db_cursor = None
+    try:
+        db_conn = engine.raw_connection()
+        db_cursor = db_conn.cursor(cursor_factory = RealDictCursor)
+        logging.info("Databse connection opened.")
+        yield db_cursor
+    finally:
+        if db_cursor:
+            db_cursor.close()
+        if db_conn:
+            db_conn.commit()
+            db_conn.close()
+        logging.info("Database connection is closed.")
+            
